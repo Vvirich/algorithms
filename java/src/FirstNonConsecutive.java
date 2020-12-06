c8f5ee17129c96738529f56e8d17251b38b29523
Vvirich <virichbog@gmail.com>
2020-12-06 13:04:36 +0200

    Add fir non consecutive algoritm:

--- java/src/FirstNonConsecutive.java
+++ java/src/FirstNonConsecutive.java
@@ -1,8 +1,4 @@
 public class FirstNonConsecutive {
-    public static void main(String[] args) {
-        System.out.println(find(new int[]{1, 2, 3, 4, 5, 6, 7, 8}));
-    }
-
     static Integer find(final int[] array) {
         for (int i = 1; i < array.length; i++) {
             if (array[i - 1] + 1 != array[i]) {
